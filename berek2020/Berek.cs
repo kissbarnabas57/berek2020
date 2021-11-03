4487f52273d3b7c7d3cada4dfc1bfebf542dad1d
kissbarnabas57 <90251406+kissbarnabas57@users.noreply.github.com>
2021-11-03 14:49:38 +0100

    Add project files.

---
+++ berek2020/Berek.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace berek2020
+{
+  class Berek
+  {
+    private string nev;
+
+    public string Nev
+    {
+      get { return nev; }
+      set { nev = value; }
+    }
+    private string neme;
+
+    public string Neme
+    {
+      get { return neme; }
+      set { neme = value; }
+    }
+    private string reszleg;
+
+    public string Reszleg
+    {
+      get { return reszleg; }
+      set { reszleg = value; }
+    }
+    private int belepes;
+
+    public int Belepes
+    {
+      get { return belepes; }
+      set { belepes = value; }
+    }
+    private int dolgozober;
+
+    public int Dolgozober
+    {
+      get { return dolgozober; }
+      set { dolgozober = value; }
+    }
+
+    public Berek(string nev, string neme, string reszleg, int belepes, int dolgozober)
+    {
+      this.nev = nev;
+      this.neme = neme;
+      this.reszleg = reszleg;
+      this.belepes = belepes;
+      this.dolgozober = dolgozober;
+    }
+  }
+}
---
+++ berek2020/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace berek2020
+{
+  public partial class Form1 : Form
+  {
+    List<Berek> lista = new List<Berek>();
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Shown(object sender, EventArgs e)
+    {
+      Beolvasas();
+      HarmadikFeladat();
+      NegyedikFeladat();
+      OtodikFeladat();
+      HatodikFeladat();
+      HetedikFeladat();
+  
+    }
+
+    private void HetedikFeladat()
+    {
+      Dictionary<string, int> stat = new Dictionary<string, int>();
+
+      stat.Add("beszerzés", 17);
+      stat.Add("pénzügy", 36);
+      stat.Add("asztalosműhely", 18);
+      stat.Add("értékesítés", 16);
+      stat.Add("lakatosműhely", 15);
+      stat.Add("karbantartás", 26);
+      stat.Add("szerelőműhely", 30);
+      stat.Add("személyzeti", 12);
+
+      
+    }
+
+    private void HatodikFeladat()
+    {
+      int max;
+      for (int i = 0; i < lista.Count; i++)
+      {
+        if ()
+        {
+
+        }
+      }
+    }
+
+    private void OtodikFeladat()
+    {
+      
+    }
+
+    private void NegyedikFeladat()
+    {
+      int sum = 0;
+      foreach (var l in lista)
+      {
+       
+      }
+    }
+
+    private void HarmadikFeladat()
+    {
+      lblDolgozo.Text = $"{lista.Count } fő";
+    }
+
+    private void Beolvasas()
+    {
+      StreamReader be = new StreamReader("berek2020.txt");
+      be.ReadLine();
+      while (!be.EndOfStream)
+      {
+        string[] a = be.ReadLine().Split(';');
+        lista.Add(new Berek(a[0],a[1],a[2],Convert.ToInt32(a[3]),Convert.ToInt32(a[4])));
+      }
+      be.Close();
+    }
+  }
+}
